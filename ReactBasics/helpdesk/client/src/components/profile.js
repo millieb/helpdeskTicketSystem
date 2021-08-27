0c34f1f4d148903d893615975edc57d0c4d01627
millieb <mildred.brito@outlook.com>
2021-08-27 12:44:50 -0600

    Profile.js is now displaying a table which dynamically renders the tickets in the database.

--- ReactBasics/helpdesk/client/src/components/profile.js
+++ ReactBasics/helpdesk/client/src/components/profile.js
@@ -15,8 +15,32 @@ function Profile() {
         })
     }, []);
 
-    return <div className="ticketContainer">
-        {listOfTickets.map((value, key) => 
+    return <div className="ticket-container">
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Ticket Number</th>
+                    <th>Date</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {listOfTickets.map((value, key)=> 
+                    <tr>
+                        <td>{value.title}</td>
+                        <td>{value.description}</td>
+                        <td>{value.email}</td>
+                        <td>{value.company}</td>
+                        <td>{value.createdAt}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+
+        {/* {listOfTickets.map((value, key) => 
         { 
             return <div className="ticket">
             <div className="title">{value.title}</div>
@@ -25,7 +49,9 @@ function Profile() {
             <div className="footer">{value.company}</div>
             <div className="footer">{value.createdAt}</div>
             </div>;
-        })}
+        })} */}
+
+
     </div>
 }
 
